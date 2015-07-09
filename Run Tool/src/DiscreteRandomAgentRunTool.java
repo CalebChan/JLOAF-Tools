d52c127b489a3cdb32bfa55a47e48af07f3fe322
calebchan <calebchan@cmail.carleton.ca>
2015-07-09 18:09:46 -0400

    - Fixed bugs to tool - Added new commands

--- Run Tool/src/DiscreteRandomAgentRunTool.java
+++ Run Tool/src/DiscreteRandomAgentRunTool.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +12,11 @@ import org.jLOAF.casebase.CaseRun;
 import org.jLOAF.inputs.AtomicInput;
 import org.jLOAF.inputs.ComplexInput;
 import org.jLOAF.inputs.Input;
+import org.jLOAF.reasoning.BacktrackingReasoning;
+import org.jLOAF.reasoning.BestRunReasoning;
+import org.jLOAF.reasoning.KNNBacktracking;
+import org.jLOAF.retrieve.kNNUtil;
+import org.jLOAF.retrieve.sequence.weight.LinearWeightFunction;
 import org.jLOAF.sim.atomic.ActionEquality;
 import org.jLOAF.sim.atomic.InputEquality;
 import org.jLOAF.sim.complex.ActionMean;
@@ -27,8 +34,13 @@ public class DiscreteRandomAgentRunTool {
 	
 	private InputActionHashMap inputActionMap;
 	
+	private InputActionHashMap testingActionMapList[];
+	
+	private FailPointList failList;
+	
 	private static final String EXIT_STRING = "exit";
 	private static final String HELP_STRING = "help";
+	
 	
 	private int total;
 	
@@ -38,6 +50,7 @@ public class DiscreteRandomAgentRunTool {
 		this.runSize = runSize;
 		
 		this.inputActionMap = new InputActionHashMap();
+		this.failList = new FailPointList();
 	}
 	
 	public void setup(){
@@ -49,15 +62,22 @@ public class DiscreteRandomAgentRunTool {
 		
 		AtomicInput.setClassStrategy(new InputEquality());;
 		ComplexInput.setClassStrategy(new InputMean());
+		
+		kNNUtil.setWeightFunction(new LinearWeightFunction(0.05));
+		
+		this.testingActionMapList = new InputActionHashMap[pair.size()];
 	}
 	
 	public void runTool(){
+		int index = 0;
 		for (TestingTrainingPair ttp : pair){
 			CaseRun r = ttp.getTesting();
 			for (int i = 0; i < r.getRunLength(); i++){
 				Case c = r.getCasePastOffset(i);
 				this.inputActionMap.put(c.getInput(), c.getAction());
 			}
+			buildTestingList(index);
+			index++;
 		}
 		
 		for (Input ahm : this.inputActionMap.keySet()){
@@ -65,31 +85,77 @@ public class DiscreteRandomAgentRunTool {
 		}
 	}
 	
+	private void buildTestingList(int runNum){
+		CaseRun run = pair.get(runNum).getTesting();
+		InputActionHashMap map = new InputActionHashMap();
+		for (int i = 0; i < run.getRunLength(); i++){
+			Case c = run.getCasePastOffset(i);
+			map.put(c.getInput(), c.getAction());
+		}
+		this.testingActionMapList[runNum] = map;
+	}
+	
 	public void displayToolSummary(){
 		String msg = "";
 		Scanner s = new Scanner(System.in);
 		helpFunction();
 		while(!msg.equals(EXIT_STRING)){
-			msg = s.nextLine();
-			String tokens[] = msg.split("-");
-			switch(tokens[0]){
-			case EXIT_STRING:
-				break;
-			case HELP_STRING:
+			try{
+				msg = s.nextLine();
+				String tokens[] = msg.split(" ");
+				switch(tokens[0]){
+				case EXIT_STRING:
+					break;
+				case HELP_STRING:
+					helpFunction();
+					break;
+				case "summary":
+				{
+					int index = 0;
+					if (tokens.length > 2){
+						index = Integer.parseInt(tokens[2]);
+					}
+					InputActionHashMap map = (tokens[1].equals("train")) ? inputActionMap : testingActionMapList[index];
+					int length = (tokens[1].equals("train")) ? total : pair.get(index).getTesting().getRunLength();
+					summary(map, length);
+					break;
+				}
+				case "all":
+				{
+					int index = 0;
+					if (tokens.length > 2){
+						index = Integer.parseInt(tokens[2]);
+					}
+					InputActionHashMap map = (tokens[1].equals("train")) ? inputActionMap : testingActionMapList[index];
+					int length = (tokens[1].equals("train")) ? total : pair.get(index).getTesting().getRunLength();
+					all(map, length);
+					break;
+				}
+				case "find":
+				{
+					String searchStr = "";
+					for (int i = 1; i < tokens.length; i++){
+						searchStr += tokens[i] + " ";
+					}
+					find(searchStr.trim());
+					break;
+				}
+				case "test":
+				{
+					int k = Integer.parseInt(tokens[2]);
+					testAll(tokens[1], k);
+					break;
+				}
+				case "track":{
+					trackFailPoint();
+					break;
+				}
+				default:
+					helpFunction();
+					break;
+				}
+			}catch(Exception e){
 				helpFunction();
-				break;
-			case "summary":
-				summary();
-				break;
-			case "all":
-				all();
-				break;
-			case "find":
-				find(tokens[1]);
-				break;
-			default:
-				helpFunction();
-				break;
 			}
 		}
 		s.close();
@@ -107,27 +173,151 @@ public class DiscreteRandomAgentRunTool {
 		}
 	}
 	
-	private void summary(){
-		System.out.println("Entry : " + this.inputActionMap.size());
-		for (Input ahm : this.inputActionMap.keySet()){
-			int sum = this.inputActionMap.getInputSummaryTotal(ahm);
-			int depth = this.inputActionMap.get(ahm).size();
+	private void summary(InputActionHashMap map, int total){
+		System.out.println("Total : " + total);
+		for (Input ahm : map.keySet()){
+			int sum = map.getInputSummaryTotal(ahm);
+			int depth = map.get(ahm).size();
 			System.out.println(ahm.getSimpleString() + " -> " + String.format("%5d", sum)+ "\t" + depth + "\t % " + String.format("%2.4f", sum / (1.0 * total) * 100));
 		}
 	}
 	
-	private void all(){
-		for (Input ahm : this.inputActionMap.keySet()){
-			for (Action action : this.inputActionMap.get(ahm).keySet()){
-				int sum = this.inputActionMap.get(ahm).get(action);
+	private void all(InputActionHashMap map, int total){
+		for (Input ahm : map.keySet()){
+			for (Action action : map.get(ahm).keySet()){
+				int sum = map.get(ahm).get(action);
 				
 				System.out.println(ahm.getSimpleString() + "= " + action.getSimpleString() + " -> " + String.format("%5d", sum) + "\t % " + String.format("%2.2f", sum / (1.0 * total) * 100));
 			}
 		}
 	}
 	
+	private void trackFailPoint(){
+		ArrayList<Integer> points = new ArrayList<Integer>(this.failList.keySet());
+		Collections.sort(points);
+		
+		for(int i : points){
+			if (this.failList.get(i).size() != 7){
+				continue;
+			}
+			ArrayList<ActionPair> actions = this.failList.get(i);
+			System.out.print("" + i + "\t");
+			for (ActionPair a : actions){
+				System.out.print("" + a + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	private HashMap<Integer, Integer> testReasoning(String reason, int test, int k){
+		BacktrackingReasoning reasoning = null;
+		if (reason.equals("knn")){
+			reasoning = new KNNBacktracking(pair.get(test).getTraining(), null, k, true, true);
+		}else if (reason.equals("best")){
+			reasoning = new BestRunReasoning(pair.get(test).getTraining(), k, true);
+		}else{
+			System.out.println("Failed to test");
+			return new HashMap<Integer, Integer>();
+		}
+		HashMap<Integer, Integer> errorMap = new HashMap<Integer, Integer>();
+		CaseRun run = new CaseRun("tmpRun");
+		
+		reasoning.setCurrentRun(run);
+		
+		CaseRun testRun = pair.get(test).getTesting();
+		for (int i = testRun.getRunLength() - 1; i >= 0; i--){
+			Case c = testRun.getCasePastOffset(i);
+			run.addCaseToRun(new Case(c.getInput(), null, run.getCurrentCase()));
+			Action a = reasoning.selectAction(c.getInput());
+			if (!a.equals(c.getAction())){
+				this.failList.traceFailPoint(run.getRunLength(), a, c.getAction());
+				if (errorMap.containsKey(run.getRunLength())){
+					errorMap.put(run.getRunLength(), errorMap.get(run.getRunLength()) + 1);
+				}else{
+					errorMap.put(run.getRunLength(), 1);
+				}
+			}
+			Case newCase = new Case(c.getInput(), c.getAction(), run.getCurrentCase().getPreviousCase());
+			run.amendCurrentCase(newCase);
+		}
+		return errorMap;
+	}
+	
+	private void testAll(String reason, int k){
+		HashMap<Integer, Integer> errorMap = new HashMap<Integer, Integer>();
+		this.failList = new FailPointList();
+		for (int test = 0; test < pair.size(); test++){
+			HashMap<Integer, Integer> tmpMap = testReasoning(reason, test, k);
+			for (Integer i : tmpMap.keySet()){
+				if (errorMap.containsKey(i)){
+					errorMap.put(i, tmpMap.get(i) + errorMap.get(i));
+				}else{
+					errorMap.put(i, tmpMap.get(i));
+				}
+			}
+		}
+		int totalErrors = 0;
+		int totalRight = 0;
+		for (int i = 0; i < 1000; i++){
+			int errors = 0;
+			if (errorMap.containsKey(i)){
+				errors = errorMap.get(i).intValue();
+				totalRight += pair.size() - errors;
+			}else{
+				totalRight += pair.size();
+			}
+			System.out.println("" + i + " --- " + errors + " ---> " + String.format("%3.5f", errors * 1.0 / 7));
+			totalErrors += errors;
+		}
+		System.out.println("Errors : " + String.format("%3.5f", totalErrors * 1.0 / total) + " " + totalErrors);
+		System.out.println("Right : " + totalRight + " Pairs : " + pair.size());
+	}
+	
+	
+	
 	private void helpFunction(){
-		System.out.println("Help");
+		System.out.println("Help Commands : ");
+		System.out.println("\t all       (train | test testNo)");
+		System.out.println("\t summary   (train | test testNo)");
+		System.out.println("\t find      searchString");
+		System.out.println("\t test      (best | knn) kValue");
+	}
+	
+	class ActionPair{
+		private Action guessAction;
+		private Action actualAction;
+		public Action getGuessAction() {
+			return guessAction;
+		}
+		public void setGuessAction(Action guessAction) {
+			this.guessAction = guessAction;
+		}
+		public Action getActualAction() {
+			return actualAction;
+		}
+		public void setActualAction(Action actualAction) {
+			this.actualAction = actualAction;
+		}
+		
+		public String toString(){
+			return guessAction.getSimpleString() + "->" + actualAction.getSimpleString();
+		}
+	}
+	class FailPointList extends HashMap<Integer, ArrayList<ActionPair>>{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public void traceFailPoint(int index, Action guessAction, Action actualAction){
+			if (!this.containsKey(index)){
+				this.put(index, new ArrayList<ActionPair>());
+			}
+			ActionPair pair = new ActionPair();
+			pair.setActualAction(actualAction);
+			pair.setGuessAction(guessAction);
+			this.get(index).add(pair);
+		}
 	}
 	
 	class ActionHashMap extends HashMap<Action, Integer>{
--- Run Tool/src/RunTool.java
+++ Run Tool/src/RunTool.java
@@ -6,9 +6,10 @@ public class RunTool {
 	
 	public static void main(String args[]){
 		String file = "C:/Users/calebchan/Desktop/Stuff/workspace/Test Data/Batch Test 3/TB/Expert/Run 1/SmartRandomAgent";
+		//String file = "C:/Users/calebchan/Desktop/Stuff/workspace/Test Data/Batch Test 3/TB/Expert/Run 1/SmartRandomExplorerAgent";
 		SmartRandomExpertStrategy expert = new SmartRandomExpertStrategy();
 		expert.parseFile(file, "ToolCasebase");
-		DiscreteRandomAgentRunTool tool = new DiscreteRandomAgentRunTool("ToolCasebase.cb", 1000, 7);
+		DiscreteRandomAgentRunTool tool = new DiscreteRandomAgentRunTool("ToolCasebase.cb", 7, 1000);
 		tool.setup();
 		tool.runTool();
 		tool.displayToolSummary();
